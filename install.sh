621c08da1eebd994fd29e775d5c95c8fcee916f8
Navar0ne <102163282+Navar0ne@users.noreply.github.com>
2025-03-11 11:05:21 +0000

    Update install.sh
    Updated install.sh to work for Ubuntu 22.04 using PHP 8.3

--- install.sh
+++ install.sh
@@ -11,42 +11,39 @@ if [[ "$EUID" -ne 0 ]]
 fi
 
 ##### Start script
-echo "###########################################################"
-echo "This script will install LibreNMS using NGINX webserver, developed for Ubuntu 20.04 LTS"
+echo "\n\n###########################################################\n\n"
+echo "This script will install LibreNMS using NGINX webserver, developed for Ubuntu 22.04 LTS"
 echo "The script will perform apt install and update commands."
 echo "Use at your own risk"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 read -p "Please [Enter] to continue..." ignore
 
 ##### Installing Required Packages
 apt install -y software-properties-common
-add-apt-repository universe
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 apt update
 echo "Upgrading installed packages"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 apt upgrade -y
 echo "Installing LibreNMS required packages"
-echo "###########################################################"
-apt install -y acl curl composer fping git graphviz imagemagick mariadb-client \
-mariadb-server mtr-tiny nginx-full nmap php7.4-cli php7.4-curl php7.4-fpm \
-php7.4-gd php7.4-json php7.4-mbstring php7.4-mysql php7.4-snmp php7.4-xml \
-php7.4-zip rrdtool snmp snmpd whois unzip python3-pymysql python3-dotenv \
-python3-redis python3-setuptools
+echo "\n\n###########################################################\n\n"
+apt install -y acl curl fping git graphviz imagemagick mariadb-client mariadb-server mtr-tiny nginx-full nmap php-cli php-curl php-fpm php-gd php-gmp php-json php-mbstring php-mysql php-snmp php-xml php-zip rrdtool snmp snmpd unzip python3-pymysql python3-dotenv python3-redis python3-setuptools python3-psutil python3-systemd python3-pip whois traceroute
+
 
 ##### Add librenms user
 echo "Add librenms user"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # add user link home directory, do not create home directory
 useradd librenms -d /opt/librenms -M -r -s "$(which bash)"
 
 ##### Download LibreNMS itself
 echo "Downloading libreNMS to /opt/librenms"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 cd /opt
 git clone https://github.com/librenms/librenms.git
 # Set permissions
 echo "Setting permissions and file access controls"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # set owner:group recursively on directory
 chown -R librenms:librenms /opt/librenms
 # mod permission on directory O=All,G=All, Oth=view
@@ -58,21 +55,21 @@ setfacl -R -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstra
 
 ##### Install PHP dependencies
 echo "Install PHP dependencies"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # run php dependencies installer
 su librenms bash -c '/opt/librenms/scripts/composer_wrapper.php install --no-dev'
 
 ##### Set system timezone
 echo "Setup of system and PHP timezone"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # Asking for timezome of choice
 echo "We have to set the system time zone."
 echo "You will get a list of all available time zones"
-echo "Use q to quit the list and enter your choice"
 read -p "Please [Enter] to continue..." ignore
 echo "-----------------------------"
 echo " "
-timedatectl list-timezones
+timedatectl list-timezones > timezones.txt
+cat timezones.txt
 echo " "
 echo "Enter system time zone of choice:"
 read TZ
@@ -80,13 +77,13 @@ timedatectl set-timezone $TZ
 # Set timezone
 echo "Setting PHP time zone"
 echo "Changing to $TZ"
-echo "################################################################################"
-sed -i "/;date.timezone =/ a date.timezone = $TZ" /etc/php/7.4/fpm/php.ini
-sed -i "/;date.timezone =/ a date.timezone = $TZ" /etc/php/7.4/cli/php.ini
+echo "\n\n################################################################################\n\n"
+sed -i "/;date.timezone =/ a date.timezone = $TZ" /etc/php/8.3/fpm/php.ini
+sed -i "/;date.timezone =/ a date.timezone = $TZ" /etc/php/8.3/cli/php.ini
 
 ##### Configure MariaDB
 echo "Configuring MariaDB"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 ##### Within the [mysqld] section of the config file please add: ####
 ## innodb_file_per_table=1
 ## lower_case_table_names=0
@@ -98,9 +95,9 @@ systemctl restart mariadb
 # Pass commands to mysql and create DB, user, and privlages
 echo "Please create a password for LibreNMS database user on MariaDB - you need it later during web installation:"
 read ANS
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 echo "######### MySQL DB:librenms Password:$ANS #################"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 sleep 3
 mysql -uroot -e "CREATE DATABASE librenms CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 mysql -uroot -e "CREATE USER 'librenms'@'localhost' IDENTIFIED BY '$ANS';"
@@ -109,19 +106,19 @@ mysql -uroot -e "FLUSH PRIVILEGES;"
 
 ##### Configure PHP-FPM
 echo "Configure PHP-FPM (FastCGI Process Manager)"
-echo "###########################################################"
-cp /etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/librenms.conf
-sed -i 's/^\[www\]/\[librenms\]/' /etc/php/7.4/fpm/pool.d/librenms.conf
-sed -i 's/^user = www-data/user = librenms/' /etc/php/7.4/fpm/pool.d/librenms.conf
-sed -i 's/^group = www-data/group = librenms/' /etc/php/7.4/fpm/pool.d/librenms.conf
-sed -i 's/^listen =.*/listen = \/run\/php-fpm-librenms.sock/' /etc/php/7.4/fpm/pool.d/librenms.conf
+echo "\n\n###########################################################\n\n"
+cp /etc/php/8.3/fpm/pool.d/www.conf /etc/php/8.3/fpm/pool.d/librenms.conf
+sed -i 's/^\[www\]/\[librenms\]/' /etc/php/8.3/fpm/pool.d/librenms.conf
+sed -i 's/^user = www-data/user = librenms/' /etc/php/8.3/fpm/pool.d/librenms.conf
+sed -i 's/^group = www-data/group = librenms/' /etc/php/8.3/fpm/pool.d/librenms.conf
+sed -i 's/^listen =.*/listen = \/run\/php-fpm-librenms.sock/' /etc/php/8.3/fpm/pool.d/librenms.conf
 
 ##### Configure web server (NGINX
 echo "Configure web server (NGINX)"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # Create NGINX .conf file
 echo "We need to change the sever name to the current IP unless the name is resolvable /etc/nginx/conf.d/librenms.conf"
-echo "################################################################################"
+echo "\n\n################################################################################\n\n"
 echo "Enter nginx server_name [x.x.x.x or serv.examp.com]: "
 read HOSTNAME
 echo 'server {'> /etc/nginx/conf.d/librenms.conf
@@ -149,11 +146,11 @@ echo '}' >>/etc/nginx/conf.d/librenms.conf
 # remove the default site link
 rm /etc/nginx/sites-enabled/default
 systemctl restart nginx
-systemctl restart php7.4-fpm
+systemctl restart php8.3-fpm
 
 ##### Enable lnms command completion
 echo "Enable lnms command completion"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 ln -s /opt/librenms/lnms /usr/local/bin/lnms
 cp /opt/librenms/misc/lnms-completion.bash /etc/bash_completion.d/
 
@@ -174,7 +171,7 @@ systemctl restart snmpd
 
 ##### Setup Cron job
 echo "Setup LibreNMS Cron job"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 cp /opt/librenms/librenms.nonroot.cron /etc/cron.d/librenms
 
 ##### Setup logrotate config
@@ -184,7 +181,7 @@ cp /opt/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms
 
 #### Common fixes
 echo "Perform common fixes in order to help pass LibreNMS validation"
-echo "###########################################################"
+echo "\n\n###########################################################\n\n"
 # create default custom config.php in case the user needs it
 cp /opt/librenms/config.php.default /opt/librenms/config.php
 # set default LibreNMS permissions which cause most errors
@@ -198,5 +195,9 @@ sudo su librenms bash -c '/opt/librenms/scripts/github-remove -d'
 
 ##### End of installation, continue in web browser
 echo "###############################################################################################"
-echo "Naviagte to http://$HOSTNAME/install.php in you web browser to finish the installation."
+echo "Naviagte to http://$HOSTNAME/install in you web browser to finish the installation."
 echo "###############################################################################################"
+
+
+
+
